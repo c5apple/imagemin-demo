cbc25ef4e72c6f8302a2e4115b9b7a08296f4f4d
c5apple <c5apple_party@yahoo.co.jp>
2019-06-25 19:29:20 +0900

    imagemin

---
+++ index.js
@@ -0,0 +1,16 @@
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminGifsicle = require('imagemin-gifsicle');
+const imageminSvgo = require('imagemin-svgo');
+
+imagemin(['assets/*.{jpg,png,gif,svg}'], 'dist', {
+  plugins: [
+    imageminMozjpeg({ quality: 80 }),
+    imageminPngquant({ quality: [0.65, 0.8] }),
+    imageminGifsicle(),
+    imageminSvgo()
+  ]
+}).then(() => {
+  console.log('Images optimized');
+});
